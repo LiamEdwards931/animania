57b6a6dcf7e142667481e292b0c507cdb736a687
LiamEdwards931 <eddy-931@hotmail.co.uk>
2024-03-11 22:40:06 +0000

    created form for adding new address, altered the model to match the fields I need for the form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.models import User
+from .models import Address
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -38,3 +39,19 @@ class CustomPasswordChange(PasswordChangeForm):
         self.fields['old_password'].help_text = ''
         self.fields['new_password1'].help_text = ''
         self.fields['new_password2'].help_text = ''
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        door_number = forms.IntegerField(required=True)
+        model = Address
+        fields = ['door_number','street', 'city', 'state', 'country', 'postal_code']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['door_number'].widget.attrs['placeholder'] = 'Door number'
+        self.fields['street'].widget.attrs['placeholder'] = 'Street'
+        self.fields['city'].widget.attrs['placeholder'] = 'City'
+        self.fields['state'].widget.attrs['placeholder'] = 'County'
+        self.fields['country'].widget.attrs['placeholder'] = 'Country'
+        self.fields['postal_code'].widget.attrs['placeholder'] = 'Post Code'
--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    door_number = models.IntegerField(validators=[MinValueValidator(1)], null=True)
     street = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    postal_code = models.CharField(max_length=20)
+    postal_code = models.CharField(max_length=8)
