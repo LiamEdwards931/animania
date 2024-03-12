36941643f486c2e08c084d29d724e4c9dab8d454
LiamEdwards931 <eddy-931@hotmail.co.uk>
2024-03-12 18:44:19 +0000

    create full custom form for adding new products, ironed out some bugs with cloudinary not working properly

--- product/forms.py
+++ product/forms.py
@@ -17,7 +17,46 @@ class ProductForm(forms.ModelForm):
         
     ]
 
+    CATEGORY_CHOICES = [
+        ('Figures','Figures'),
+        ('Pop-Vinyl','Pop-Vinyl'),
+        ('Clothing','Clothing'),
+        ('VideoGame','VideoGame')
+    ]
+
+    SUB_CATEGORY_CHOICE = [
+        (None, None),
+        ('hats', 'Hats'),
+        ('hoodies', 'Hoodies'),
+        ('t-shirt', 'T-Shirts'),
+        ('jumper', 'Jumpers'),
+        ('backpack', 'Backpacks'),
+        ('ps4', 'PS4 Accessories'),
+        ('xbox', 'Xbox Accessories'),
+        ('soft-toy', 'Soft Toys'),
+        ('action-figures', 'Action Figures'),
+        ('collectible-statues', 'Collectible Statues'),
+        ('model-kits', 'Model Kits'),
+        ('miniatures', 'Miniatures'),
+        ('dolls', 'Dolls'),
+        ('funko-pop', 'Funko Pop!'),
+        ('vinyl-figures', 'Vinyl Figures'),
+        ('bobbleheads', 'Bobbleheads'),
+        ('jackets', 'Jackets'),
+        ('dresses', 'Dresses'),
+        ('skirts', 'Skirts'),
+        ('console-accessories', 'Console Accessories'),
+        ('gaming-merchandise', 'Gaming Merchandise'),
+        ('controller-skins', 'Controller Skins'),
+        ('gaming-chairs', 'Gaming Chairs'),
+        ('gaming-keyboards', 'Gaming Keyboards'),
+        ('gaming-mice', 'Gaming Mice'),
+    ]
+
     series = forms.ChoiceField(choices=SERIES_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    category = forms.ChoiceField(choices=CATEGORY_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    sub_category = forms.ChoiceField(choices=SUB_CATEGORY_CHOICE, widget=forms.Select(attrs={'class': 'form-control'}))
+    related_products = forms.ModelMultipleChoiceField(queryset=Product.objects.all(), required=False, widget=forms.SelectMultiple(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Product
@@ -27,7 +66,6 @@ class ProductForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control-textarea'}),
             'price': forms.NumberInput(attrs={'class': 'form-control'}),
-            'category': forms.TextInput(attrs={'class': 'form-control'}),
             'quantity_available': forms.NumberInput(attrs={'class': 'form-control'}),
             'new': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
             'discounted': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
@@ -41,5 +79,26 @@ class ProductForm(forms.ModelForm):
 
         if category == 'clothing' and not size:
             raise forms.ValidationError("Size is required for clothing items.")
+
+        return cleaned_data
+    
+    def clean(self):
+        # Appends category, series, sub_category into the search tag field in the model.
+        cleaned_data = super().clean()
+        category = cleaned_data.get('category')
+        series = cleaned_data.get('series')
+        sub_category = cleaned_data.get('sub_category')
+
+        search_tags = []
+
+        if category:
+            search_tags.append(category)
+        if series:
+            search_tags.append(series)
+        if sub_category:
+            search_tags.append(sub_category)
+
+        # Join the search tags list into a single string and assign it to the search_tags field
+        cleaned_data['search_tags'] = ', '.join(search_tags)
 
         return cleaned_data
