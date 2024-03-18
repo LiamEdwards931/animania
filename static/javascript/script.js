9e65a184e5f8bcde0aa9f25c5a5993320699b1f9
LiamEdwards931 <eddy-931@hotmail.co.uk>
2024-03-18 11:32:44 +0000

    updated style, js and html to also render the new buttons for each banner uploaded and follow the same logic that a user can click to change them

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -28,17 +28,26 @@ const messageContainer = document.getElementById('remove-message');
 
 // banner image js function that allows users to click the image to move to the next one in the list
 var images = document.querySelectorAll('.img-banner');
+var imageButton = document.querySelectorAll('.banner-form-button')
 
 document.querySelector('.banner-container').addEventListener('click', function() {
     var visibleImage = document.querySelector('.img-banner.banner-visible');
+    var visibleForm = document.querySelector('.banner-form-button.form-visible');
     var visibleIndex = Array.from(images).indexOf(visibleImage);
+    var VisibleButtonIndex = Array.from(imageButton).indexOf(visibleForm);
     var nextIndex = (visibleIndex + 1) % images.length;
+    var nextButtonIndex = (VisibleButtonIndex + 1) % imageButton.length;
     var nextImage = images[nextIndex];
+    var nextButton = imageButton[nextButtonIndex];
     
     visibleImage.classList.remove('banner-visible');
     visibleImage.classList.add('banner-hidden');
     nextImage.classList.remove('banner-hidden');
     nextImage.classList.add('banner-visible');
+    visibleForm.classList.remove('form-visible');
+    visibleForm.classList.add('form-hidden');
+    nextButton.classList.remove('form-hidden');
+    nextButton.classList.add('form-visible');
 });
 
 // circle indicators for the image banners
