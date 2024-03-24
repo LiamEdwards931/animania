f50785d4fc19161122b6ec66885d8d159295b098
LiamEdwards931 <eddy-931@hotmail.co.uk>
2024-03-24 22:14:01 +0000

    noticed type in get method in base.html search form, add the imports in the basket view,as it needed revising to do what I needed it to do

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from product.models import Product
+
 
 # Create your views here.
 
@@ -45,3 +46,53 @@ def add_to_basket(request,item_id):
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'selected_size' in request.POST:
+        size = request.POST['selected_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['products_by_size'][size] = quantity
+        else:
+            del bag[item_id]['products_by_size'][size]
+            if not bag[item_id]['products_by_size']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('basket'))
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'selected_size' in request.POST:
+            size = request.POST['selected_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['products_by_size'][size]
+            if not bag[item_id]['products_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except KeyError:
+        return HttpResponse(status=400)
+    except Exception:
+        return HttpResponse(status=500)
