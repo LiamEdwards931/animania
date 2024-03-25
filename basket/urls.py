9f425393ae44b1ded8842c4a56a4096e51eedae3
LiamEdwards931 <eddy-931@hotmail.co.uk>
2024-03-25 15:24:59 +0000

    updated the icon when basket is full, add delete basket url

--- basket/urls.py
+++ basket/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
    path('basket/', views.basket, name="basket"),
    path('add/<item_id>', views.add_to_basket, name='add_to_basket'),
    path('update/<item_id>',views.adjust_bag, name='update_basket'),
+   path('delete/<item_id>', views.remove_from_bag, name='delete_basket'),
 ]
