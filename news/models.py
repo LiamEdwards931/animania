4549f0cc30e94ab4ab27fc45605012f43f1bc40f
LiamEdwards931 <eddy-931@hotmail.co.uk>
2024-04-04 10:40:49 +0100

    created a model for news objects

--- news/models.py
+++ news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class news(models.Model):
+    title = models.CharField(max_length=15, blank=False, null=False)
+    news_image = CloudinaryField('image',blank=True)
+    short_description = models.CharField(max_length=50, blank=False, null=False)
+    description = models.CharField(max_length=1000, blank=False, null=False)
+    video_url = models.URLField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
