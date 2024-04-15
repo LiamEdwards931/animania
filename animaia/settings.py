bff4536c1c25227f0e55d9526997fbd1fd9ded45
LiamEdwards931 <eddy-931@hotmail.co.uk>
2024-04-15 16:55:07 +0100

    switched debug to False

--- animaia/settings.py
+++ animaia/settings.py
@@ -33,7 +33,7 @@ NEWS_TEMPLATES_DIR = os.path.join(BASE_DIR,'news','templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
