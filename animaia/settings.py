642a7fde660e647aec55f9ddbc61c00af391e5e9
LiamEdwards931 <eddy-931@hotmail.co.uk>
2024-05-26 14:16:15 +0100

    fixed validation issues in the settings.py file, whitespace, line too long, no newline problems

--- animaia/settings.py
+++ animaia/settings.py
@@ -18,12 +18,12 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
-PRODUCT_TEMPLATES_DIR = os.path.join(BASE_DIR,'product','templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+PRODUCT_TEMPLATES_DIR = os.path.join(BASE_DIR, 'product', 'templates')
 ACCOUNTS_TEMPLATES_DIR = os.path.join(BASE_DIR, 'accounts', 'templates')
-BASKET_TEMPLATES_DIR = os.path.join(BASE_DIR,'basket','templates')
-CHECKOUT_TEMPLATES_DIR = os.path.join(BASE_DIR,'checkout','templates')
-NEWS_TEMPLATES_DIR = os.path.join(BASE_DIR,'news','templates')
+BASKET_TEMPLATES_DIR = os.path.join(BASE_DIR, 'basket', 'templates')
+CHECKOUT_TEMPLATES_DIR = os.path.join(BASE_DIR, 'checkout', 'templates')
+NEWS_TEMPLATES_DIR = os.path.join(BASE_DIR, 'news', 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +37,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['animania-175b65d61606.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['animania-175b65d61606.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -107,8 +107,8 @@ WSGI_APPLICATION = 'animaia.wsgi.application'
 #     }
 # }
 DATABASES = {
-'default':
-dj_database_url.parse(os.environ.get("DATABASE_URL"))
+ 'default':
+ dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
@@ -117,16 +117,25 @@ dj_database_url.parse(os.environ.get("DATABASE_URL"))
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator',
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.MinimumLengthValidator',
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.CommonPasswordValidator',
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.NumericPasswordValidator',
+        ),
     },
 ]
 
@@ -149,9 +158,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = (
+    'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
@@ -167,12 +178,12 @@ LOGOUT_REDIRECT_URL = "index"
 DELIVERY_AMOUNT = 75
 DELIVERY_COST = 10
 
-# 404 
+# 404
 handler404 = 'product.views.styled_404'
 
-#Stripe
+# Stripe
 
-STRIPE_CURRENCY ='gbp'
+STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY','')
-STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET','')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
